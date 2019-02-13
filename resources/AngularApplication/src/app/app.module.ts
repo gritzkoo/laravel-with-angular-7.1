b756fe6a3e235f3929bbde2412404058fef39273
Gritzko Daniel Kleiner <gritzkoo@hotmail.com>
2019-02-13 13:45:13 -0200

    configuração do bootstrap

--- resources/AngularApplication/src/app/app.module.ts
+++ resources/AngularApplication/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { HomeComponent } from './Components/home/home.component';
 import { UsersTableComponent } from './Components/users-table/users-table.component';
-
+import 'bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
